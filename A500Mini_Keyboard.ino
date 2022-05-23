ada630a327f37b6592d4f76f9872d95adbaecb12
ea4gge <90650383+ea4gge@users.noreply.github.com>
2022-05-23 23:55:27 +0200

    Update A500Mini_Keyboard.ino
    Older in the A500Mini_Keyboard's directory, in the .bak file. Here the latest update

--- A500Mini_Keyboard.ino
+++ A500Mini_Keyboard.ino
@@ -16,7 +16,7 @@ const byte COLS = 12; // columns
 byte colPins[COLS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; //row pinouts of the keypad
 byte rowPins[ROWS] = {12, 13, 18, 19, 20, 21, 22, 23}; //column pinouts of the keypad
 //
-//define the keys of the keyboard. 3 american Amiga layout, one for normal, one for shift and one for capslock
+//define the keys of the keyboard as American Amiga layout, one for normal
 //
 char directKeys[ROWS][COLS] = {
   
@@ -26,129 +26,134 @@ char directKeys[ROWS][COLS] = {
   {KEY_LEFT_CTRL, KEY_CAPS_LOCK,'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'},
   {KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', KEY_RIGHT_SHIFT},
   {KEY_LEFT_ALT, KEY_LEFT_GUI, ' ', KEY_RIGHT_GUI, KEY_RIGHT_ALT, KEY_DELETE, KEY_PAGE_DOWN, KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_RETURN},
-  {'=', '\\', ']', '#', '(', ')', '/', '*', '-', '+', KEY_RETURN, '.'},
-  {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ' ', }
+  {'=', '\\', ']', '#', 219, ')', 220, 221, 222, 223, 224, 235},
+  {234, 225, 226, 227, 228, 229, 230, 231, 232, 233, ' ', ' ', }
  
 };
-
-char shiftKeys[ROWS][COLS] = {
-  
-  {KEY_ESC, KEY_F11, KEY_F12, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_BACKSPACE},
-  {'`', '!', '"', '£', '$', '%', '^', '&', '*', '(', ')', '_'},
-  {KEY_TAB, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{'},
-  {KEY_LEFT_CTRL, KEY_CAPS_LOCK,'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':'},
-  {KEY_LEFT_SHIFT, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', KEY_RIGHT_SHIFT},
-  {KEY_LEFT_ALT, KEY_LEFT_GUI, ' ', KEY_RIGHT_GUI, KEY_RIGHT_ALT, KEY_DELETE, KEY_PAGE_DOWN, KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_RETURN},
-  {'+', '|', '}', '@', '(', ')', '/', '*', '-', '+', KEY_RETURN, '.'},
-  {'1', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ' ', }
-
-};
-
-char capsKeys[ROWS][COLS] = {
-
-  //{KEY_ESC, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_BACKSPACE},
-//  {'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-'},
-//  {KEY_TAB, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '['},
-//  {KEY_LEFT_CTRL, KEY_CAPS_LOCK,'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';'},
-//  {KEY_LEFT_SHIFT, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', KEY_RIGHT_SHIFT},
-//  {KEY_LEFT_ALT, KEY_LEFT_GUI, ' ', KEY_RIGHT_GUI, KEY_RIGHT_ALT, KEY_DELETE, KEY_PAGE_DOWN, KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_RETURN},
-//  {'=', '\\', ']', '#', '(', ')', '/', '*', '-', '+', KEY_RETURN, '.'},
-//  {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ' ', }
-
-  {KEY_ESC, KEY_F11, KEY_F12, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_BACKSPACE},
-  {'`', '!', '"', '£', '$', '%', '^', '&', '*', '(', ')', '_'},
-  {KEY_TAB, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{'},
-  {KEY_LEFT_CTRL, KEY_CAPS_LOCK,'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':'},
-  {KEY_LEFT_SHIFT, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', KEY_RIGHT_SHIFT},
-  {KEY_LEFT_ALT, KEY_LEFT_GUI, ' ', KEY_RIGHT_GUI, KEY_RIGHT_ALT, KEY_DELETE, KEY_PAGE_DOWN, KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_RETURN},
-  {'+', '|', '}', '@', '(', ')', '/', '*', '-', '+', KEY_RETURN, '.'},
-  {'1', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ' ', }
-
-};
-// Create the 3 key layouts
-Keypad directLayout( makeKeymap(directKeys), rowPins, colPins, sizeof(rowPins), sizeof(colPins) );
-Keypad shiftLayout( makeKeymap(shiftKeys), rowPins, colPins, sizeof(rowPins), sizeof(colPins) );
-Keypad capsLayout( makeKeymap(capsKeys), rowPins, colPins, sizeof(rowPins), sizeof(colPins) );
 
 byte Layout = 0;
 char Key = 0;
 byte State = 0;
 const bool KeybInit = true;
 
-
-//Setup 
+Keypad kpd = Keypad( makeKeymap(directKeys), rowPins, colPins, ROWS, COLS );
+unsigned long loopCount;
+unsigned long startTime;
 
 void setup() {
   Keyboard.begin();
-  
-  
-  directLayout.begin( makeKeymap(directKeys) );
-  shiftLayout.begin( makeKeymap(shiftKeys) );
-  capsLayout.begin( makeKeymap(capsKeys) );
+    loopCount = 0;
+    startTime = millis();
 
   // KeybInit
   if (KeybInit) {
  
   }
-       }
+}
 
 void loop() {
-  getKey();
-}
+    loopCount++;
+    if ( (millis()-startTime)>5000 ) {
+        startTime = millis();
+        loopCount = 0;
+    }
+    
+    // Fills kpd.key[ ] array with up-to 10 active keys.
+    // Returns true if there are ANY active keys.
+    if (kpd.getKeys())
+    {
+        for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
+        {
+            if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+            {
+                switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+                    case PRESSED:
+               Keyboard.press(kpd.key[i].kchar);
+               break;
 
-void respondToKey(char key, byte state) {
-  switch (state) {
-    case PRESSED:
-     
-      if (key == 1) {
-        Layout++;
-        key = 0;
-        delay(400);
-      } 
-      else {
-        if (Layout == 3) {
+                    case HOLD:
+                Keyboard.press(kpd.key[i].kchar);
+                break;
 
-        Keyboard.press(KEY_LEFT_CTRL); 
-        if ((key == KEY_RIGHT_ARROW)||(key == KEY_LEFT_ARROW)||(key == KEY_UP_ARROW)||(key == KEY_DOWN_ARROW)){
-        Keyboard.press(KEY_LEFT_SHIFT);
+                    case RELEASED:
+               Keyboard.releaseAll();
+               for (int i=0; i<LIST_MAX; i++){
+                if (kpd.key[i].kchar == char(KEY_LEFT_SHIFT)) {
+               
+               Keyboard.press(KEY_LEFT_SHIFT);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }
+                if (kpd.key[i].kchar == char(KEY_RIGHT_SHIFT)) {
+               
+               Keyboard.press(KEY_RIGHT_SHIFT);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }
+                if (kpd.key[i].kchar == char(KEY_LEFT_CTRL)) {
+               
+               Keyboard.press(KEY_LEFT_CTRL);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }
+                if (kpd.key[i].kchar == char(KEY_LEFT_CTRL)) {
+               
+               Keyboard.press(KEY_LEFT_CTRL);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }
+/*                if (kpd.key[i].kchar == char(KEY_LEFT_GUI)) {
+               
+               Keyboard.press(KEY_LEFT_GUI);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }
+                if (kpd.key[i].kchar == char(KEY_RIGHT_GUI)) {
+               
+               Keyboard.press(KEY_RIGHT_GUI);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }*/
+                if (kpd.key[i].kchar == char(KEY_LEFT_ALT)) {
+               
+               Keyboard.press(KEY_LEFT_ALT);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }
+                if (kpd.key[i].kchar == char(KEY_RIGHT_ALT)) {
+               
+               Keyboard.press(KEY_RIGHT_ALT);
+               switch (kpd.key[i].kstate) {
+                      case RELEASED:
+               Keyboard.releaseAll();
+               }
+               }
+
+              }
+                break;
+                  
+               
+                    case IDLE:
+ //               Keyboard.releaseAll();        
+                  break;
                    
-        }
-          else
-          {    
+                }
                 
-          }
+            }
         }
-        Keyboard.press(key);
-      }
-      break;
-    case HOLD:
-      break;
-    case RELEASED:
-      Keyboard.releaseAll();
-      break;
-  }
-}
-
-
-void getKey() {
-  switch (Layout) {
-    case 0:
-      Key = directLayout.getKey();
-      State = directLayout.getState();
-      break;
-    case 1:
-      Key = shiftLayout.getKey();
-      State = shiftLayout.getState();
-      break;
-    case 2:
-      Key = capsLayout.getKey();
-      State = capsLayout.getState();
-       break;
-    default:
-      Layout = 0;
-      State = 0;
-  }
-
-  respondToKey(Key, State);
-
+    }
 }
